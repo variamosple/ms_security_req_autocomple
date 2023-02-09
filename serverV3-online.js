a7987f688ee8e8775b220976234c3ae4d173a817
Jairo Soto <jasotov@eafit.edu.co>
2023-02-09 08:36:37 -0500

    Increased the version

--- serverV3-online.js
+++ serverV3-online.js
@@ -542,7 +542,7 @@ app.post('/suggest', async function (req, res, next) {
 });
 
 app.get('/version', async function (req, res, next) {
-    let version="1.23.02.09.08"
+    let version="1.23.02.09.09"
     res.end(JSON.stringify({ "version": version }));
 });
 
